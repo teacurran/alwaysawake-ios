d3212072d6676c1d9f233185b96fc2155cfdc482
Terrence Curran <tcurran@approachingpi.com>
2016-02-15 20:37:58 -0500

    initial project structure

---
+++ alwaysawake/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  alwaysawake
+//
+//  Created by Terrence Curran on 2/15/16.
+//  Copyright (c) 2016 Approaching Pi. All rights reserved.
+//
+
+
+import UIKit
+
+
+class FirstViewController: UIViewController {
+
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
