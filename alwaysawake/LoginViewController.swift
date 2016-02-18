c0bdb85d9adfef343f55cb870e1ebb85ae07bf8c
Terrence Curran <tcurran@approachingpi.com>
2016-02-17 22:45:21 -0500

    started working on login

---
+++ alwaysawake/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+// Created by Terrence Curran on 2/17/16.
+// Copyright (c) 2016 Approaching Pi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+}
